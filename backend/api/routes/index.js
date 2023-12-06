365cd87072f693e3067568740dda00e4672d065e
Vero <veroramirezmoreno@gmail.com>
2023-12-06 12:20:34 +0000

    routes - index.js updated

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,11 +1,22 @@
 const router = require("express").Router()
-const userRouter = require("./user.router")
 const authRouter = require("./auth.router")
+const userRouter = require("./user.router")
+const projectRouter = require("./project.router")
+const agendaRouter = require("./agenda.router")
+const invoiceRouter = require("./invoice.router")
+const chat_messageRouter = require("./chat_message.router")
+
+
+
 
 const { checkAuth } = require("../middlewares/index")
 
 router
         .use("/auth", authRouter)
         .use("/user", checkAuth, userRouter)
+        .use("/project", checkAuth,projectRouter)
+        .use("/agenda", checkAuth,agendaRouter)
+        .use("/invoice", checkAuth,invoiceRouter)
+        .use("/message", checkAuth,chat_messageRouter)       
 
 module.exports = router
