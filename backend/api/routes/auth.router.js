defe4509fce137a683e0a7791471d687172ff8e1
jmorma3 <jmorma3@gmail.com>
2023-12-08 13:23:02 +0000

    Backend login and signup endpoints fixed. Frontend working fine!

--- backend/api/routes/auth.router.js
+++ backend/api/routes/auth.router.js
@@ -1,14 +1,13 @@
 const router = require("express").Router()
 
-const { 
-   signUp, 
-   logIn
+const {
+    signUp,
+    logIn
 } = require("../controllers/auth.controller")
 
 router
-    .post("/client/signup", signUp)
-    .post("/admin/login", logIn)
-    .post("/client/login", logIn)
-    .post("/developer/login", logIn)
+    .post("/login", logIn)
+    .post("/signup", signUp)
+    
 
 module.exports = router
--- frontend/waveweb/src/services/authService.js
+++ frontend/waveweb/src/services/authService.js
@@ -2,23 +2,12 @@ import api from "./config";
 
 const login = async (body) => {
     try {
-      const  data  = await api.post("/auth/admin/login", body)
+      const  data  = await api.post("/auth/login", body)
       localStorage.setItem('token', data.token)
       console.log(data)
       return 200
     } catch (error) {
-        if (error.response) {
-          // El servidor respondió con un código de estado diferente de 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // La solicitud fue hecha pero no se recibió respuesta
-          console.log(error.request);
-        } else {
-          // Algo sucedió en la configuración de la solicitud que generó un error
-          console.log('Error', error.message);
-        }
+       console.log(error)
       }
   }
   
