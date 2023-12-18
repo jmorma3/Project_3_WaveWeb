8b3be17e87b77801ed0519d94fc57a000f77ca8b
jmorma3 <jmorma3@gmail.com>
2023-12-18 20:28:07 +0000

    New baseURL added to config.js (frontend)

--- frontend/waveweb/src/services/config.js
+++ frontend/waveweb/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "mysql://root:DCBd33CG4Hf1hdHH1AcEhFCD5ceF3dfF@monorail.proxy.rlwy.net:23694/railway"
+    baseURL: "http://project3waveweb-production.up.railway.app/api"
 })
 
 export default api
